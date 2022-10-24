1c69e07db1e50a829b9fde68a285156f3064db2b
Anastasia Karsten <anastasia.karsten@student.uni-augsburg.de>
2022-10-24 21:30:56 +0200

    Clarify the bounds order in the help string

--- plot.py
+++ plot.py
@@ -6,16 +6,24 @@ from TwoD import TwoD
 @click.command()
 @click.argument('dim', type=int)
 @click.argument('bounds', type=str)
-# @click.option('-d', '--dim', type=int, default=1, prompt='Please enter the number of dimensions:',
+# @click.option('-d', '--dim', type=int, default=1,
+#               prompt='Please enter the number of dimensions:',
 #               help='Soft Integral dimensions')
-# @click.option('-b', '--bounds', type=str, prompt='Please enter the classifier bounds:',
+# @click.option('-b', '--bounds', type=str,
+#               prompt='Please enter the classifier bounds:',
 #               help='The lower and upper bounds of the classifier. Ex. "0.2, 0.3"')
-@click.option('--n_points', type=int, default=100, help='How many points should be rendered?')
-@click.option('--range', type=str, default="0. 1.", help='Graph range. Eg.: \"0., 1.\"')
-@click.option('--show_hard', type=bool, default=False, help='Should the hard interval be shown too? True/False')
+@click.option('--n_points', type=int, default=100,
+              help='How many points should be rendered?')
+@click.option('--range', type=str, default="0. 1.",
+              help='Graph range. Eg.: \"0., 1.\"')
+@click.option('--show_hard', type=bool, default=False,
+              help='Should the hard interval be shown too? True/False')
 def plot(dim, bounds, n_points, range, show_hard):
-    """Prints a graph of the Soft Interval for a DIM-dimensional Classifier with BOUNDS.
-    Please enter BOUNDS in quotation marks. Ex.: \"0.2 0.3\" """
+    """Prints a graph of the Soft Interval for a DIM-dimensional
+    Classifier with BOUNDS. Please enter BOUNDS in quotation marks.
+    Ex.: \"0.2 0.3\". For multi dimensional bounds please enter the
+    lower and upper bound of each axis together. Ex. for 2D:
+    \"lower-x upper-x lower-y upper-y\""""
 
     bounds = tuple(map(float, bounds.split()))
     range = tuple(map(float, range.split()))
